11812e06b06860037c226a4b085f804082756ee8
Jack Coble <jack@coble.uk>
2021-01-09 20:32:53 +0000

    Fix errors

--- src/models/coin/coin.ts
+++ src/models/coin/coin.ts
@@ -1,49 +1,49 @@
 import { Feirm } from "./coins/feirm";
 
 // Model types for a coin and their network parameters
-export interface ICoin {
+export interface Coin {
     name: string;
     ticker: string;
     
     // BIP44 type
     coinType: number;
     
-    network: INetwork
+    network: Network;
 }
 
-export interface INetwork {
+export interface Network {
     messagePrefix: string;
     bech32: string;
-    bip32: IBip32;
+    bip32: Bip32;
     pubKeyHash: number;
     scriptHash: number;
     wif: number;
 }
 
-interface IBip32 {
+interface Bip32 {
     public: number;
     private: number;
 }
 
 // Class for coin factory
 export class CoinFactory {
-    static coins: { [ticker: string]: ICoin } = {
+    static coins: { [ticker: string]: Coin } = {
         XFE: new Feirm()
     }
 
     // Coin list
-    static coinsList: ICoin[] = [
+    static coinsList: Coin[] = [
         CoinFactory.coins.XFE
     ]
 
     // Fetch a coin by its ticker
-    static getCoin(ticker: string): ICoin {
+    static getCoin(ticker: string): Coin {
         ticker = ticker.toUpperCase();
         return this.coins[ticker];
     }
 
     // Fetch all available coins
-    static getAllCoins(): ICoin[] {
+    static getAllCoins(): Coin[] {
         return this.coinsList;
     }
 }
--- src/models/coin/coins/feirm.ts
+++ src/models/coin/coins/feirm.ts
@@ -1,18 +1,18 @@
-import { ICoin } from "@/models/coin/coin";
+import { Coin } from "@/models/coin/coin";
 
-export class Feirm implements ICoin {
-  "name": "Feirm";
-  "ticker": "XFE";
-  "coinType": 193;
-  "network": {
-    "messagePrefix": "\x18DarkNet Signed Message:\n",
-    "bech32": "",
-    "bip32": {
-      "public": 0x0488b21e,
-      "private": 0x0488ade4
+export class Feirm implements Coin {
+  name = "Feirm";
+  ticker = "XFE";
+  coinType = 193;
+  network = {
+    messagePrefix: "\x18DarkNet Signed Message:\n",
+    bech32: "",
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4,
     },
-    "pubKeyHash": 12,
-    "scriptHash": 57,
-    "wif": 55
-  }
+    pubKeyHash: 12,
+    scriptHash: 57,
+    wif: 55
+  };
 }
