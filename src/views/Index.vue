562c8fb6b85f9745ee5ccfb4a844dc6374d5a0f4
Jack Coble <jack@coble.uk>
2021-01-09 17:54:40 +0000

    Cleanup template project

---
+++ src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="title">Feirm Recovery Tool</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Index',
+});
+</script>
