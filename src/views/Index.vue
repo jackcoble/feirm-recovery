a90c73e17c446b17b5400d982ffc19f6bd609ccd
Jack Coble <jack@coble.uk>
2021-01-09 19:56:08 +0000

    Validate and store mnemonic in Vuex

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -11,11 +11,14 @@
                   <p>Recover cryptocurrency private keys from your BIP39 mnemonic phrase.</p>
                 </div>
 
+                <!-- Invalid mnemonic error -->
+                <invalid-mnemonic v-show="invalidMnemonic"></invalid-mnemonic>
+
                 <!-- Mnemonic entry -->
                 <div class="field">
                   <label class="label">Mnemonic phrase</label>
                   <div class="control">
-                    <input class="input" type="text" placeholder="Mnemonic phrase">
+                    <input class="input" type="text" placeholder="Mnemonic phrase" v-model="mnemonic">
                   </div>
                 </div>
 
@@ -35,7 +38,7 @@
                 <hr>
                 <div class="field">
                   <div class="control">
-                    <button class="button is-warning">Recover</button>
+                    <button class="button is-warning" @click="storeMnemonic(mnemonic)">Recover</button>
                   </div>
                 </div>
               </div>
@@ -54,19 +57,26 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { validateMnemonic } from "bip39";
 
 // Components
 import Liability from "@/components/Liability.vue";
+import InvalidMnemonic from "@/components/InvalidMnemonic.vue";
+
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "Index",
   data() {
     return {
+      mnemonic: "",
       disclaimerModal: false,
+      invalidMnemonic: false
     };
   },
   components: {
     Liability,
+    InvalidMnemonic
   },
   mounted() {
     // Open the disclaimer modal
@@ -76,6 +86,28 @@ export default defineComponent({
     showDisclaimer() {
       this.disclaimerModal = !this.disclaimerModal;
     },
+    storeMnemonic(mnemonic: string) {
+      // Validate the mnemonic
+      const valid = validateMnemonic(mnemonic)
+
+      // Show a popup error if the mnemonic isn't valid
+      if (!valid) {
+        return this.invalidMnemonic = !this.invalidMnemonic;
+      }
+
+      // Mnemonic is valid
+      this.invalidMnemonic = false;
+
+      // Store the mnemonic in Vuex
+      this.store.commit("setMnemonic", mnemonic);
+    }
   },
+  setup() {
+    const store = useStore();
+
+    return {
+      store
+    }
+  }
 });
 </script>
