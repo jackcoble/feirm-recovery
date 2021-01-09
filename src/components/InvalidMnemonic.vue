3e1f122948be71c46d9d23671ce52ef432127a79
Jack Coble <jack@coble.uk>
2021-01-09 19:55:51 +0000

    Invalid mnemonic component

---
+++ src/components/InvalidMnemonic.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="notification is-danger has-text-centered">
+        The mnemonic you provided is not valid! Please try again.
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "InvalidMnemonic"
+})
+</script>
