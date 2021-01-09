868c682a324dedf23472c2d629cb60ba795a6ed0
Jack Coble <jack@coble.uk>
2021-01-09 18:55:15 +0000

    Navigation bar component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
+  <navigation />
   <router-view/>
 </template>
+
+<script lang="ts">
+import Navigation from "@/components/Navigation.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Navigation
+  }
+})
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav
+    class="navbar is-spaced is-dark"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="container">
+      <div class="navbar-brand">
+        <a href="/" class="branding"></a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Navigation"
+})
+</script>
+
+<style scoped>
+.branding {
+    background: url("../assets/img/navbar-logo.png") no-repeat center center;
+    background-size: cover;
+    width: 152px
+}
+</style>
