71b2655a6098bc494f98ee6e08f1048064bb217a
Jack Coble <jack@coble.uk>
2021-01-09 19:40:57 +0000

    Vuex store for mnemonic

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    mnemonic: ""
   },
   mutations: {
+    setMnemonic(state, mnemonic) {
+      state.mnemonic = mnemonic;
+    }
   },
-  actions: {
-  },
-  modules: {
+  getters: {
+    getMnemonic: state => state.mnemonic
   }
 })
